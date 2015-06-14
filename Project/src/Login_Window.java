b2f0123c547659c51a7c6142f96f7cf3a5852c2e
Kim <Kim@128.189.245.240>
2015-06-13 18:25:58 -0700

    6:24PM changed password field to be blackdotted 	both functions (Login + CreateAccount) works fine witht he password field
    Player1 table is now displayed as a separate window after you can log in

---
+++ Project/src/Login_Window.java
@@ -0,0 +1,215 @@
+package src;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JList;
+import java.awt.Choice;
+
+
+
+public class Login_Window extends JFrame {
+	private boolean bVisible = false;
+	private JPanel contentPane;
+	private JTextField txtName;
+	private static Statement stmt;
+	static Connection connection;
+	private JPasswordField psfID;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login_Window frame = new Login_Window();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				// connection to ORACLE
+				try {
+					DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+					connection = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug", "ora_i6k8", "a21014121");
+					stmt = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE, ResultSet.HOLD_CURSORS_OVER_COMMIT);
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					//Hello There;
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login_Window() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				try {
+					connection.close();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setBounds(144, 65, 182, 28);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		txtName.requestFocus();
+		
+		final JLabel lblError = new JLabel("");
+		final JButton btnLoging = new JButton("Log In");
+		
+		// Drop down menu
+		final Choice ddmServer = new Choice();
+		ddmServer.setVisible(false);
+		ddmServer.setEnabled(false);
+		ddmServer.setBounds(144, 217, 182, 26);
+		ddmServer.add("North America 1");
+		ddmServer.add("North America 2");
+		ddmServer.add("Asia");
+		ddmServer.add("Europe");
+		
+		lblError.setBounds(20, 37, 404, 16);
+		contentPane.add(lblError);
+		
+		final JButton btnNewPlayer = new JButton("Create an account");
+		btnNewPlayer.addMouseListener(new MouseAdapter() {
+			@Override
+			// when click on create new player button (CreateNewPlayer)
+			public void mouseClicked(MouseEvent e) {
+				// check
+				if (bVisible)
+				{
+					//check if name and playerID fields are empty
+					if ((!txtName.getText().isEmpty()) || (!(psfID.getPassword().length==0))){
+						try {
+							// check if playerID is integer
+							int iID = 0;
+							try {
+								int tempPass = Integer.parseInt(String.valueOf(psfID.getPassword()));	
+								stmt.executeQuery("INSERT INTO Player1 VALUES ('" + txtName.getText() + "','" + tempPass + "')");
+								lblError.setText("Success");
+								}
+							catch (NumberFormatException numError)
+							{
+								lblError.setText("Please Enter an integer value as PlayerID");
+							}
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							if (e1.getErrorCode() == 1)
+								lblError.setText("The PlayerID is already taken please choose another.");
+							else
+								e1.printStackTrace();
+						}
+					}
+					else
+					{
+						lblError.setText("Enter Somethin");
+					}
+				}
+				else
+				{
+					ddmServer.setEnabled(true);
+					ddmServer.setVisible(true);
+					ddmServer.setBounds(btnLoging.getBounds().x, btnLoging.getBounds().y, btnLoging.getBounds().width, btnLoging.getBounds().height);
+					btnNewPlayer.setBounds(btnNewPlayer.getBounds().x, btnNewPlayer.getBounds().y+50, btnNewPlayer.getBounds().width, btnNewPlayer.getBounds().height);
+					btnLoging.setBounds(btnLoging.getBounds().x, btnLoging.getBounds().y+50, btnLoging.getBounds().width, btnLoging.getBounds().height);
+					bVisible = true;
+				}
+			}
+		});
+		btnNewPlayer.setBounds(144, 180, 182, 29);
+		contentPane.add(btnNewPlayer);
+		
+		btnLoging.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				try {
+					String username = txtName.getText();
+					char[] charPassword = psfID.getPassword();
+					String password = String.valueOf(charPassword);
+					String sql = "SELECT AccountName,AccountID FROM Player1 WHERE AccountName='" + username + "' AND AccountID=" + password;                    
+					ResultSet rs = stmt.executeQuery(sql);
+					
+					int count = 0;
+					while(rs.next()) {
+						count = count + 1;
+					}
+					if (count == 1 ) {
+						JOptionPane.showMessageDialog(null, "Access Granted!");
+						dispose();
+						stmt.close();
+						rs.close();
+						new Main_Window(connection).setVisible(true);
+						
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Access Denied.");
+						txtName.setText("");
+						psfID.setText("");
+						txtName.requestFocus();
+					}
+				}
+				catch (SQLException e1) {
+					JOptionPane.showMessageDialog(null, "Access Denied.");
+					txtName.setText("");
+					psfID.setText("");
+					txtName.requestFocus();
+				}
+			}
+		});
+		btnLoging.setBounds(144, 145, 182, 29);
+		contentPane.add(btnLoging);
+		
+		JLabel lblPlayerName = new JLabel("Player Name :");
+		lblPlayerName.setBounds(52, 71, 94, 16);
+		contentPane.add(lblPlayerName);
+		
+		JLabel lblPlayerId = new JLabel("Player ID :");
+		lblPlayerId.setBounds(52, 111, 94, 16);
+		contentPane.add(lblPlayerId);
+		
+		psfID = new JPasswordField();
+		psfID.setColumns(10);
+		psfID.setBounds(144, 105, 182, 28);
+		contentPane.add(psfID);
+		contentPane.add(ddmServer);
+		
+
+	}
+}
--- Project/src/Main_Window.java
+++ Project/src/Main_Window.java
@@ -1,5 +1,6 @@
-import javax.swing.JFrame;
+package src;
 
+import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 
 import java.awt.BorderLayout;
@@ -7,22 +8,33 @@ import java.awt.BorderLayout;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JTable;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-=======
+import java.sql.Statement;
+import java.util.Vector;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 import java.awt.GridLayout;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JOptionPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class Main_Window extends JFrame{
 	private JTable table;
 	
-	public Main_Window(){
+	public Main_Window(final Connection connection){
 		
 		JScrollPane scrollPane = new JScrollPane();
 		getContentPane().add(scrollPane, BorderLayout.CENTER);
@@ -74,86 +86,83 @@ public class Main_Window extends JFrame{
 		setSize(707,364);
 		getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(426, 27, 275, 208);
-		getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblInfo = new JLabel("Player Info:");
-		lblInfo.setBounds(6, 6, 70, 16);
-		panel.add(lblInfo);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(null);
-		panel_1.setBounds(16, 37, 253, 144);
-		panel.add(panel_1);
-		
-		JLabel label = new JLabel("");
-		label.setBounds(251, 0, 70, 48);
-		
-		JLabel label_1 = new JLabel("");
-		label_1.setBounds(265, 85, 70, 48);
-		panel_1.setLayout(null);
-		panel_1.add(label);
-		panel_1.add(label_1);
-		
-		JLabel lblAccountId = new JLabel("Account ID:");
-		lblAccountId.setBounds(6, 0, 96, 20);
-		panel_1.add(lblAccountId);
-		
-		JLabel lblGuild = new JLabel("Guild: ");
-		lblGuild.setBounds(6, 30, 41, 20);
-		panel_1.add(lblGuild);
-		
-		JLabel lblAccountName = new JLabel("Account Name:");
-		lblAccountName.setBounds(6, 60, 119, 20);
-		panel_1.add(lblAccountName);
-		
-		JLabel lblServer = new JLabel("Server:");
-		lblServer.setBounds(6, 90, 70, 20);
-		panel_1.add(lblServer);
-		setVisible(true);
-		
-		ResultSet rs = null;
-		int totalCol = 0;
-		int totalRow = getTotalRow(rs);
+		//Fetch Data from the database
 		try {
-			totalCol = ((ResultSetMetaData) rs).getColumnCount();
-		} catch (SQLException e) {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from Player1");
+			JTable table = new JTable(buildTableModel(rs));
+			JScrollPane scrollTable = new JScrollPane(table);
+			JFrame frame = new JFrame("Testing Table");
+			frame.getContentPane().add(scrollTable, BorderLayout.CENTER);
+		    frame.setSize(300, 150);
+		    frame.setVisible(true);
+//			JOptionPane.showMessageDialog(null, new JScrollPane(table));
+			
+		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-		Object rows[][] = new Object[totalRow][totalCol];
-		Object headers[] = new Object[totalCol];
-		try {
-			while(rs.next()) {
-				for(int i=0; i<totalRow++; i++) {
-					for(int j=0; j<totalCol++; j++) {
-						rows[i][j] = rs.getString(j); 
-					}
-				}
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+//		TO BE WORKED ON
+		//Button for showing table Player1
+//		JButton btnGetTableplayer = new JButton("Get Table \"Player1\"");
+//		btnGetTableplayer.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//				displayTable(connection);
+//			}
+//		});
+//		btnGetTableplayer.setBounds(39, 74, 200, 50);
+//		getContentPane().add(btnGetTableplayer);
+		
+		
 		
 	}
 	
-	public int getTotalRow(ResultSet rs) {
-		int row =0;
-		try {
-			while(!rs.isLast()) {
-				rs.next();
-			}
-			row = rs.getRow();
-		}
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return row;
+//	TO BE WORKED ON
+//	public void displayTable(Connection connection) {
+//				//Fetch Data from the database
+//				try {
+//					Statement stmt = connection.createStatement();
+//					ResultSet rs = stmt.executeQuery("select * from Player1");
+//					JTable table = new JTable(buildTableModel(rs));
+//					JScrollPane scrollTable = new JScrollPane(table);
+//					JFrame frame = new JFrame("Testing Table");
+//					frame.getContentPane().add(scrollTable, BorderLayout.CENTER);
+//				    frame.setSize(300, 150);
+//				    frame.setVisible(true);
+//					JOptionPane.showMessageDialog(null, new JScrollPane(table));
+//					
+//				} catch (SQLException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+//	}
+	
+	public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
+
+	    ResultSetMetaData metaData = rs.getMetaData();
+
+	    // names of columns
+	    Vector<String> columnNames = new Vector<String>();
+	    int columnCount = metaData.getColumnCount();
+	    for (int column = 1; column <= columnCount; column++) {
+	        columnNames.add(metaData.getColumnName(column));
+	    }
+
+	    // data of the table
+	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+	    while (rs.next()) {
+	        Vector<Object> vector = new Vector<Object>();
+	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+	            vector.add(rs.getObject(columnIndex));
+	        }
+	        data.add(vector);
+	    }
+
+	    return new DefaultTableModel(data, columnNames);
+
 	}
+	
 	private static class __Tmp {
 		private static void __tmp() {
 			  javax.swing.JPanel __wbp_panel = new javax.swing.JPanel();

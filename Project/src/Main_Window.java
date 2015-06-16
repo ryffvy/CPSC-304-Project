facd3f5d8638d7b897aa49d20df38ddaef6ba50c
Kim <Kim@192.168.1.69>
2015-06-15 21:46:19 -0700

    9:46pm

--- Project/src/Main_Window.java
+++ Project/src/Main_Window.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JTable;
@@ -16,18 +17,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Vector;
 
-
-
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import java.awt.GridLayout;
 
+import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
 import javax.swing.JTabbedPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.table.DefaultTableModel;
@@ -35,6 +35,8 @@ import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 
 public class Main_Window extends JFrame{
@@ -108,31 +110,12 @@ public class Main_Window extends JFrame{
 		panPlayerInfo.add(panel_5);
 		panel_5.setLayout(null);
 		
-		//Create and add the list of Characters the Player owns into the combo box
-		JLabel lblCharacter = new JLabel("Characters: ");
-		lblCharacter.setBounds(0, 6, 128, 16);
-		panel_5.add(lblCharacter);
-		String[] playerCharList = {"","","","",""};		//A player can only have up to 5 characters.
-		try {
-			ResultSet rs = stmt.executeQuery("Select * from CharacterOwned where AccountID = " + accountID);
-			int count = 0;
-			while(rs.next()){
-				playerCharList[count] = rs.getString("CharName");
-				count++;
-			}
-		}
-		catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		JComboBox comboBox = new JComboBox(playerCharList);
-		comboBox.setBounds(16, 34, 200, 27);
-		panel_5.add(comboBox);
-		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBounds(276, 6, 232, 284);
 		panPlayerInfo.add(panel_2);
 		panel_2.setLayout(null);
 		
+
 		JLabel lblCharacterInfo = new JLabel("Character Info: ");
 		lblCharacterInfo.setBounds(0, 6, 94, 16);
 		panel_2.add(lblCharacterInfo);
@@ -142,29 +125,88 @@ public class Main_Window extends JFrame{
 		panel_2.add(panel_3);
 		panel_3.setLayout(null);
 		
-		JLabel lblName = new JLabel("Name: ");
-		lblName.setBounds(6, 0, 208, 16);
+		final JLabel lblName = new JLabel("Name: ");
+		lblName.setBounds(6, 0, 44, 16);
 		panel_3.add(lblName);
+		final JLabel lblNameFill = new JLabel();
+		lblNameFill.setBounds(48, 1, 150, 16);
+		panel_3.add(lblNameFill);
 		
 		JLabel lblClass = new JLabel("Class: ");
-		lblClass.setBounds(6, 30, 208, 16);
+		lblClass.setBounds(6, 30, 44, 16);
 		panel_3.add(lblClass);
+		final JLabel lblClassFill = new JLabel();
+		lblClassFill.setBounds(48, 30, 150, 16);
+		panel_3.add(lblClassFill);
 		
-		JLabel lblLevel = new JLabel("Label: ");
-		lblLevel.setBounds(6, 60, 208, 16);
+		JLabel lblLevel = new JLabel("Level: ");
+		lblLevel.setBounds(6, 60, 44, 16);
 		panel_3.add(lblLevel);
+		final JLabel lblLevelFill = new JLabel();
+		lblLevelFill.setBounds(48, 60, 150, 16);
+		panel_3.add(lblLevelFill);
 		
 		JLabel lblRace = new JLabel("Race: ");
-		lblRace.setBounds(6, 90, 208, 16);
+		lblRace.setBounds(6, 90, 44, 16);
 		panel_3.add(lblRace);
-		
-		JLabel lblServer_1 = new JLabel("Server: ");
-		lblServer_1.setBounds(6, 120, 208, 16);
-		panel_3.add(lblServer_1);
+		final JLabel lblRaceFill = new JLabel();
+		lblRaceFill.setBounds(48, 90, 150, 16);
+		panel_3.add(lblRaceFill);
 		
 		JLabel lblGold = new JLabel("Gold: ");
-		lblGold.setBounds(6, 150, 208, 16);
+		lblGold.setBounds(6, 120, 44, 16);
 		panel_3.add(lblGold);
+		final JLabel lblGoldFill = new JLabel();
+		lblGoldFill.setBounds(48, 120, 150, 16);
+		panel_3.add(lblGoldFill);
+		
+		//Create and add the list of Characters the Player owns into the combo box
+		JLabel lblCharacter = new JLabel("Characters: ");
+		lblCharacter.setBounds(6, 6, 128, 16);
+		panel_5.add(lblCharacter);
+		String[] playerCharList = {"(No character selected)","","","","",""};		//A player can only have up to 5 characters.
+															//+1 space to show an empty space when the application is first opened
+		try {
+			ResultSet rs = stmt.executeQuery("Select * from CharacterOwned where AccountID = " + accountID);
+			int count = 1;
+			while(rs.next()){
+				playerCharList[count] = rs.getString("CharName");
+				count++;
+			}
+		}
+		catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		//Fill in the information of the selected Character
+		final JComboBox comboBox = new JComboBox(playerCharList);
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				String selectedChar = (String) comboBox.getSelectedItem();
+				if(!(selectedChar.equals("(No character selected)"))) {
+					try {
+						ResultSet rs = stmt.executeQuery("Select * from CharacterOwned CO where CharName='"+ selectedChar + "'");
+						while(rs.next()) {
+							lblNameFill.setText(selectedChar);
+							lblClassFill.setText(rs.getString("Class"));
+							lblLevelFill.setText(rs.getString("Levels"));
+							lblRaceFill.setText(rs.getString("Race"));
+//							lblGoldFill.setText(rs.getString("Gold"));
+						}
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+				else {
+					lblNameFill.setText("");
+					lblClassFill.setText("");
+					lblLevelFill.setText("");
+					lblRaceFill.setText("");
+					lblGoldFill.setText("");
+				}
+			}
+		});
+		comboBox.setBounds(22, 34, 200, 27);
+		panel_5.add(comboBox);
 		
 		//View a specified Character's inventory
 		JButton btnViewInventory = new JButton("View Inventory");
@@ -181,12 +223,164 @@ public class Main_Window extends JFrame{
 		btnViewInventory.setBounds(534, 105, 136, 50);
 		panPlayerInfo.add(btnViewInventory);
 		
-		JPanel tabGuild = new JPanel();
-		tabbedPane.addTab("Guild", null, tabGuild, null);
 		
+		//Create a Guild tab
+		JPanel tabGuild1 = new JPanel();
+		tabbedPane.addTab("Guild", null, tabGuild1, null);
+		tabGuild1.setLayout(null);
+		
+		JLabel lblGuild_1 = new JLabel("Guild: ");
+		lblGuild_1.setBounds(17, 6, 41, 23);
+		tabGuild1.add(lblGuild_1);
+		
+		JComboBox listGuildBox = new JComboBox();
+		listGuildBox.setBounds(57, 5, 200, 25);
+		tabGuild1.add(listGuildBox);
+		
+//		JPanel panel_7 = new JPanel();
+//		panel_7.setBounds(17, 41, 242, 121);
+//		tabGuild1.add(panel_7);
+//		panel_7.setLayout(null);
+		
+		JLabel lblGuildLeader = new JLabel("GuildLeader:");
+		lblGuildLeader.setBounds(17, 40, 200, 22);
+		tabGuild1.add(lblGuildLeader);
+//		lblGuildLeader.setBounds(0, 0, 200, 22);
+//		panel_7.add(lblGuildLeader);
+		
+		JLabel lblMembers = new JLabel("# Members:");
+//		lblMembers.setBounds(0, 30, 200, 22);
+		lblMembers.setBounds(17, 70, 200, 22);
+		tabGuild1.add(lblMembers);
+//		panel_7.add(lblMembers);
+		
+		JLabel lblGuildServer = new JLabel("Server:");
+//		lblGuildServer.setBounds(0, 60, 200, 22);
+		lblGuildServer.setBounds(17, 100, 200, 22);
+		tabGuild1.add(lblGuildServer);
+//		panel_7.add(lblGuildServer);
+		
+		JLabel lblRosterSize = new JLabel("Roster Size:");
+//		lblRosterSize.setBounds(0, 90, 200, 22);
+		lblRosterSize.setBounds(17, 130, 200, 22);
+		tabGuild1.add(lblRosterSize);
+//		panel_7.add(lblRosterSize);
+
+		
+		JButton btnJoin = new JButton("Join Guild");
+		btnJoin.setBounds(17, 167, 115, 23);
+		tabGuild1.add(btnJoin);
+		
+		JButton btnLeave = new JButton("Leave Guild");
+		btnLeave.setBounds(140, 167, 115, 23);
+		tabGuild1.add(btnLeave);
+		
+		JLabel lblGuildMembers = new JLabel("Guild Members:");
+		lblGuildMembers.setBounds(277, 6, 200, 23);
+		tabGuild1.add(lblGuildMembers);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(277, 41, 393, 148);
+		tabGuild1.add(scrollPane_2);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBounds(29, 196, 641, 104);
+		tabGuild1.add(panel_8);
+		panel_8.setLayout(null);
+		
+		JButton btnAddPlayer = new JButton("Add Chosen Player");
+		btnAddPlayer.setBounds(0, 6, 172, 29);
+		panel_8.add(btnAddPlayer);
+		
+		JLabel lblToGuild = new JLabel("to Guild: ");
+		lblToGuild.setBounds(10, 47, 65, 16);
+		panel_8.add(lblToGuild);
+		
+		JComboBox toGuildBox = new JComboBox();
+		toGuildBox.setBounds(62, 45, 177, 25);
+		panel_8.add(toGuildBox);
+		
+		JScrollPane scrollPane_3 = new JScrollPane();
+		scrollPane_3.setBounds(247, 22, 388, 76);
+		panel_8.add(scrollPane_3);
+		
+		JLabel lblP = new JLabel("Players in All Guilds:");
+		lblP.setBounds(247, 6, 200, 16);
+		panel_8.add(lblP);
+		
+		
+		//Create an Admin tab
 		JPanel tabAdmin = new JPanel();
+		tabAdmin.setVisible(false);
 		tabbedPane.addTab("Admin", null, tabAdmin, null);
+		tabAdmin.setLayout(null);
 		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBounds(6, 6, 197, 250);
+		tabAdmin.add(panel_4);
+		panel_4.setLayout(null);
+		
+		JLabel lblDisplayOptions = new JLabel("Available Options:");
+		lblDisplayOptions.setBounds(6, 0, 140, 16);
+		panel_4.add(lblDisplayOptions);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBounds(16, 17, 175, 214);
+		panel_4.add(panel_6);
+		panel_6.setLayout(null);
+		
+		ButtonGroup bg = new ButtonGroup();
+		
+		JRadioButton rdbtnCharacters = new JRadioButton("# of Characters");
+		rdbtnCharacters.setSelected(true);
+		rdbtnCharacters.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
+		rdbtnCharacters.setBounds(0, 0, 150, 30);
+		panel_6.add(rdbtnCharacters);
+		bg.add(rdbtnCharacters);
+		
+		JRadioButton rdbtnByServer = new JRadioButton("By server: ");
+		rdbtnByServer.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
+		rdbtnByServer.setBounds(0, 22, 150, 30);
+		panel_6.add(rdbtnByServer);
+		bg.add(rdbtnByServer);
+		
+		JComboBox byServerBox = new JComboBox();
+		byServerBox.setBounds(29, 52, 140, 22);
+		panel_6.add(byServerBox);
+		
+		JRadioButton rdbtnPlayersWith = new JRadioButton("<html>Players with > 2 characters and Level greater than: </html>");
+		rdbtnPlayersWith.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
+		rdbtnPlayersWith.setBounds(0, 78, 175, 37);
+		panel_6.add(rdbtnPlayersWith);
+		bg.add(rdbtnPlayersWith);
+		
+		JComboBox levelBox = new JComboBox();
+		levelBox.setBounds(29, 120, 75, 22);
+		panel_6.add(levelBox);
+		
+		JRadioButton rdbtnBestPurchaser = new JRadioButton("Best Purchaser");
+		rdbtnBestPurchaser.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
+		rdbtnBestPurchaser.setBounds(0, 146, 150, 30);
+		panel_6.add(rdbtnBestPurchaser);
+		bg.add(rdbtnBestPurchaser);
+		
+		JRadioButton rdbtnPlayerWithMost = new JRadioButton("Player With Most Gold");
+		rdbtnPlayerWithMost.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
+		rdbtnPlayerWithMost.setBounds(0, 174, 150, 30);
+		panel_6.add(rdbtnPlayerWithMost);
+		bg.add(rdbtnPlayerWithMost);
+
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(215, 6, 445, 250);
+		tabAdmin.add(scrollPane_1);
+		
+		JButton btnShowPlayers = new JButton("Show Players");
+		btnShowPlayers.setBounds(6, 262, 200, 28);
+		tabAdmin.add(btnShowPlayers);
+		
+		JButton btnNewButton = new JButton("Delete Player");
+		btnNewButton.setBounds(506, 262, 154, 28);
+		tabAdmin.add(btnNewButton);
 		
 	    //Display Player Info
 		try {			
